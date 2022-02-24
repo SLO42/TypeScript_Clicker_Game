3ac86265173c00600f1b9d383afdea478505d36a
SLO42 <saolivei@student.42.us.org>
2022-02-23 20:58:43 -0600

    connected routes and started to expand api

---
+++ server/src/routes/auth/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (_req,res) => {
+	res.send("hello at auth");
+});
+
+
+
+
+export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,10 @@ import express from "express";
 
 import db from "./db/db";
 import { config } from "./config";
-import dotenv from 'dotenv';
-import cors from 'cors';
-import helmet from 'helmet';
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import APIRouter from "./routes";
 
 // .env
 dotenv.config();
@@ -15,33 +16,36 @@ server.enable("trust proxy");
 
 // CORS
 server.use(
-    cors({
-        origin: (origin, callback) => {
-            if (
-                config.accessControlAllowOrigin === "*" ||
+	cors({
+		origin: (origin, callback) => {
+			if (
+				config.accessControlAllowOrigin === "*" ||
                 origin === config.accessControlAllowOrigin ||
                 !origin
-            ){
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-    }),
+			){
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
+	}),
 );
 
 // Helmet
 server.use(helmet());
 
-server.get('/_ah/warmup', async (_, res) => {
-    try {
-      // Test successful DB Connection
-        await db.raw('select 1 as dbIsConnected');
-        res.sendStatus(200);
-    } catch (error) {
-        console.error(error)
-        res.sendStatus(500);
-    }
-});
 
-server.listen(config.port, ()=> {console.log(`Server listening on port ${config.port}`)});
+// Routes
+server.get("/_db/warmup", async (_, res) => {
+	try {
+		// Test successful DB Connection
+		await db.raw("select 1 as dbIsConnected");
+		res.sendStatus(200);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+	}
+});
+server.use("/api", APIRouter);
+
+server.listen(config.port, ()=> {console.log(`Server listening on port ${config.port}`);});
